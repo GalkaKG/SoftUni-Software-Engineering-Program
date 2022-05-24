1a04dc91f6871c49fb7679f3244f850dba036089
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2022-05-24 10:09:52 +0300

    Add files via upload

---
+++ Programming Basics with Python/Conditional Statements - Exercise/bonus_score.py
@@ -0,0 +1,21 @@
+number = int(input())
+bonus = 0
+
+if number <= 100:
+    bonus = 5
+elif number > 1000:
+    bonus = number * 0.10
+else:
+    bonus = number * 0.20
+
+if number % 2 == 0:
+    bonus += 1
+elif number % 10 == 5:
+    bonus += 2
+
+total = number + bonus
+
+print(bonus)
+print(total)
+
+
