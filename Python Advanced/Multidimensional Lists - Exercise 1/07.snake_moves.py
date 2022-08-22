d06c5649e72ad88a7d9c2314e49df8ee42c52348
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2022-08-22 18:22:41 +0300

    Update 07.snake_moves.py

--- Python Advanced/Multidimensional Lists - Exercise 1/07.snake_moves.py
+++ Python Advanced/Multidimensional Lists - Exercise 1/07.snake_moves.py
@@ -1,3 +1,24 @@
+# first decision:
+
+rows, cols = [int(x) for x in input().split()]
+word = input()
+
+index = 0
+
+for row in range(rows):
+    elements = [None] * cols
+    start, end, step = (0, cols, 1) if row % 2 == 0 else (cols -1, -1, -1)
+    for col in range(start, end, step):
+        elements[col] = word[index % len(word)]
+        index += 1
+    print(''.join(elements))
+
+
+
+    
+
+# second decision:
+
 rows, cols = [int(x) for x in input().split()]
 word = input()
 
