5d675cce02dc10b6b5e3a2d755e19bfd0d3c44cf
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2022-09-02 14:37:58 +0300

    Create 01.shortest_path_in_unweighted_graph.py

---
+++ Algorithms (with Python) Fundamentals - Exercises/Graphs Shortest Path and MST - Lab/01.shortest_path_in_unweighted_graph.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+nodes = int(input())
+edges = int(input())
+
+graph = []
+[graph.append([]) for _ in range(nodes + 1)]
+
+for _ in range(edges):
+    source, destination = [int(x) for x in input().split()]
+    graph[source].append(destination)
+
+start_node = int(input())
+destination_node = int(input())
+
+visited = [False] * (nodes + 1)
+parent = [None] * (nodes + 1)
+
+visited[start_node] = True
+queue = deque([start_node])
+
+while queue:
+    node = queue.popleft()
+    if node == destination_node:
+        break
+    for child in graph[node]:
+        if visited[child]:
+            continue
+        visited[child] = True
+        queue.append(child)
+        parent[child] = node
+
+path = deque()
+node = destination_node
+while node is not None:
+    path.appendleft(node)
+    node = parent[node]
+
+print(f'Shortest path length is: {len(path) - 1}')
+print(*path, sep=" ")
