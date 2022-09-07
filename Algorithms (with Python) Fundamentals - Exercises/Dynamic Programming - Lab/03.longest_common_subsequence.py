966f2bcc206a2c713fed8a315f7deafe12ab8649
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2022-09-07 10:01:14 +0300

    Create 03.longest_common_subsequence.py

---
+++ Algorithms (with Python) Fundamentals - Exercises/Dynamic Programming - Lab/03.longest_common_subsequence.py
@@ -0,0 +1,17 @@
+first = input()
+second = input()
+
+rows = len(first) + 1
+cols = len(second) + 1
+
+dp = []
+[dp.append([0] * cols) for _ in range(rows)]
+
+for row in range(1, rows):
+    for col in range(1, cols):
+        if first[row - 1] == second[col - 1]:
+            dp[row][col] = dp[row - 1][col - 1] + 1
+        else:
+            dp[row][col] = max(dp[row - 1][col], dp[row][col - 1])
+
+print(dp[rows - 1][cols - 1])
