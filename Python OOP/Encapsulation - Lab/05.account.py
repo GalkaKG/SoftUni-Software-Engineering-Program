67192c7604c610ffa9aeba5fb390b86189b51f89
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2022-09-08 23:44:50 +0300

    Create 05.account.py

---
+++ Python OOP/Encapsulation - Lab/05.account.py
@@ -0,0 +1,16 @@
+class Account:
+    def __init__(self, id, balance, pin):
+        self.__id = id
+        self.balance = balance
+        self.__pin = pin
+
+    def get_id(self, pin):
+        if pin == self.__pin:
+            return self.__id
+        return 'Wrong pin'
+
+    def change_pin(self, old_pin, new_pin):
+        if self.__pin == old_pin:
+            self.__pin = new_pin
+            return 'Pin changed'
+        return 'Wrong pin'
