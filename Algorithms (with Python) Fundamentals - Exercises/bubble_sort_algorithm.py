d8982b8d89eb247e3e52896824be1bb17a038ee1
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2022-09-09 22:01:09 +0300

    Create bubble_sort_algorithm.py

---
+++ Algorithms (with Python) Fundamentals - Exercises/bubble_sort_algorithm.py
@@ -0,0 +1,10 @@
+def bubble_sort(lst):
+    for i in range(len(lst)):
+        for j in range(len(lst) - 1 - i):
+            if lst[j] > lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+    return lst
+
+
+lst = [12, 1, 8, 17, 2]
+print(bubble_sort(lst))
