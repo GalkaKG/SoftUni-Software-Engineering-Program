08f9767d7a30b341d3b57bdac48347c460f9df96
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2022-09-13 16:27:44 +0300

    Create 04.numbers_filter.py

---
+++ Python Advanced/Functions Advanced - Exercise/04.numbers_filter.py
@@ -0,0 +1,9 @@
+def even_odd_filter(**kwargs):
+    result = {}
+
+    for key, value in kwargs.items():
+        parity = 0 if key == 'even' else 1
+        filtered = [x for x in value if x % 2 == parity]
+        result[key] = filtered
+
+    return result
