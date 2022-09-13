6424f67890d9f02454fbb8a77a821f58f74167d9
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2022-09-13 22:19:17 +0300

    Create 07.grocery.py

---
+++ Python Advanced/Functions Advanced - Exercise/07.grocery.py
@@ -0,0 +1,3 @@
+def grocery_store(**kwargs):
+    sorted_result = [f'{key}: {value}' for key, value in sorted(kwargs.items(), key=lambda x: (-x[1], -len(x[0]), x[0]))]
+    return '\n'.join(sorted_result)
