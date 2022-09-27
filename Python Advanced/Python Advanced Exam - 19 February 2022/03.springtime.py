ea1971e75814021bbaaf84c10b5616b4f6e96b9e
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2022-09-27 13:14:40 +0300

    Create 03.springtime.py

---
+++ Python Advanced/Python Advanced Exam - 19 February 2022/03.springtime.py
@@ -0,0 +1,10 @@
+def start_spring(**kwargs):
+    collection_dict, print_result = {}, ''
+    for value, key in kwargs.items():
+        collection_dict[key] = collection_dict.get(key, []) + [value]
+    for k, v in sorted(collection_dict.items(), key=lambda x: (-len(x[1]), x[0])):
+        print_result += f'{k}:\n'
+        for el in sorted(v):
+            print_result += f'-{el}\n'
+            
+    return print_result
