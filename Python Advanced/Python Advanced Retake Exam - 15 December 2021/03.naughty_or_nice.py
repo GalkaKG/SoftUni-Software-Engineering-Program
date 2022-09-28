2cd02db03bd37aca5510c4244b34dc2b021fd09e
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2022-09-28 14:56:42 +0300

    Create 03.naughty_or_nice.py

---
+++ Python Advanced/Python Advanced Retake Exam - 15 December 2021/03.naughty_or_nice.py
@@ -0,0 +1,36 @@
+def naughty_or_nice_list(*args, **kwargs):
+    santa_dict = {'Nice': [], 'Naughty': [], 'Not found': []}
+    list_with_tuples = args[0]
+    args = list(args)[1:]
+    for arg in args:
+        count = 0
+        pair = ()
+        num, type_ = arg.split('-')
+        for t in list_with_tuples:
+            if t[0] == int(num):
+                count += 1
+                pair = t
+                name_kid = t[1]
+        if count == 1:
+            santa_dict[type_].append(name_kid)
+            list_with_tuples.remove(pair)
+    for name_, type_ in kwargs.items():
+        count = 0
+        pair = ()
+        for t in list_with_tuples:
+            if t[1] == name_:
+                count += 1
+                pair = t
+                name_kid = t[1]
+        if count == 1:
+            santa_dict[type_].append(name_kid)
+            list_with_tuples.remove(pair)
+    if len(list_with_tuples) > 0:
+        for info in list_with_tuples:
+            santa_dict['Not found'].append(info[1])
+            # list_with_tuples.remove(info)
+    print_result = ''
+    for k, v in santa_dict.items():
+        if len(v) > 0:
+            print_result += f'{k}: {", ".join(v)}\n'
+    return print_result
