8e89f694fb0ef02e80a510654fbd638b58089018
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2022-10-04 00:02:38 +0300

    Update 04.file_delete.py

--- Python Advanced/File Handling Lab/04.file_delete.py
+++ Python Advanced/File Handling Lab/04.file_delete.py
@@ -1,3 +1,6 @@
 import os
 
-os.remove('../03.file_writer/my_first_file.txt')
+try:
+    os.remove('../03.file_writer/my_first_file.txt')
+except FileNotFoundError:
+    print('File already deleted')
