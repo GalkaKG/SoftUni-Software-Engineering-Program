c486c16e81b76769bb8d700092753f7b752d33c5
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2022-10-20 23:48:20 +0300

    Update 03.magic_triangle.py

--- Python Advanced/Python Advanced Retake Exam - 16 December 2020/03.magic_triangle.py
+++ Python Advanced/Python Advanced Retake Exam - 16 December 2020/03.magic_triangle.py
@@ -1,3 +1,28 @@
+# 1-st decision:
+
+def get_magic_triangle(n):
+    triangle = []
+    for row in range(n):
+        curr_nums = []
+        idx = -1
+        for col in range(row + 1):
+            if col == 0:
+                curr_nums.append(1)
+            elif col == row:
+                curr_nums.append(1)
+            else:
+                idx += 1
+                num = triangle[row - 1][idx] + triangle[row - 1][idx + 1]
+                curr_nums.append(num)
+        triangle.append(curr_nums)
+        
+    return triangle
+
+
+
+
+# 2-nd decision:
+
 def get_magic_triangle(n):
     result = []  # [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]  for n = 5
     # 1 + n - x  - Formula
