cc946f6082c9c495bc81653e65a93e6b4c42c68d
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2022-10-24 22:18:23 +0300

    Create 03.employee.py

---
+++ Python OOP/First Steps in OOP - Exercise/03.employee.py
@@ -0,0 +1,23 @@
+class Employee:
+    def __init__(self, id, first_name, last_name, salary):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.salary = salary
+
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def get_annual_salary(self):
+        new_salary = self.salary * 12
+        return new_salary
+
+    def raise_salary(self, amount):
+        self.salary += amount
+        return self.salary
+
+
+employee = Employee(744423129, "John", "Smith", 1000)
+print(employee.get_full_name())
+print(employee.raise_salary(500))
+print(employee.get_annual_salary())
