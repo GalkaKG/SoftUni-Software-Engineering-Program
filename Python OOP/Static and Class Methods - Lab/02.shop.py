15eca024c661188a99ddaf3073bf8729f68cc7c6
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2022-10-31 17:02:47 +0200

    Create 02.shop.py

---
+++ Python OOP/Static and Class Methods - Lab/02.shop.py
@@ -0,0 +1,30 @@
+class Shop:
+    def __init__(self, name, type, capacity):
+        self.name = name
+        self.type = type
+        self.capacity = capacity
+        self.items = {}
+
+    @classmethod
+    def small_shop(cls, name, type):
+        return cls(name, type, 10)
+
+    def add_item(self, item_name):
+        if self.capacity > sum(self.items.values()):
+            if item_name not in self.items:
+                self.items[item_name] = 0
+            self.items[item_name] += 1
+            return f"{item_name} added to the shop"
+        return "Not enough capacity in the shop"
+
+    def remove_item(self, item_name, amount):
+        if item_name in self.items:
+            if amount <= self.items[item_name]:
+                self.items[item_name] -= amount
+                if self.items[item_name] == 0:
+                    del self.items[item_name]
+                return f"{amount} {item_name} removed from the shop"
+        return f"Cannot remove {amount} {item_name}"
+
+    def __repr__(self):
+        return f"{self.name} of type {self.type} with capacity {self.capacity}"
