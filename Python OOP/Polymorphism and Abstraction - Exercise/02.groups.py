a891ced7c65f99483c63f6c82f31ab50517db0cd
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2022-11-01 17:47:27 +0200

    Create 02.groups.py

---
+++ Python OOP/Polymorphism and Abstraction - Exercise/02.groups.py
@@ -0,0 +1,47 @@
+class Person:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
+
+    def __add__(self, other):
+        return Person(self.name, other.surname)
+
+
+class Group:
+    def __init__(self, name, people):
+        self.name = name
+        self.people = people
+
+    def __len__(self):
+        return len(self.people)
+
+    def __add__(self, other):
+        return Group(f'{self.name} {other.name}', self.people + other.people)
+
+    def __getitem__(self, idx):
+        return f'Person {idx}: {str(self.people[idx])}'
+
+    def __str__(self):
+        return f"Group {self.name} with members {', '.join([str(x) for x in self.people])}"
+
+
+p0 = Person('Aliko', 'Dangote')
+p1 = Person('Bill', 'Gates')
+p2 = Person('Warren', 'Buffet')
+p3 = Person('Elon', 'Musk')
+p4 = p2 + p3
+
+first_group = Group('__VIP__', [p0, p1, p2])
+second_group = Group('Special', [p3, p4])
+third_group = first_group + second_group
+
+print(len(first_group))
+print(second_group)
+print(third_group[0])
+
+for person in third_group:
+    print(person)
+
