64c1a33e6ab85019c1c062738751b43d5f3995aa
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2022-11-16 18:26:42 +0200

    Create emails.py

---
+++ Python OOP/SOLID Exercises/emails.py
@@ -0,0 +1,70 @@
+from abc import ABC, abstractmethod
+
+
+# Single responsibility Principle
+
+class IContent(ABC):
+    def __init__(self, text):
+        self.text = text
+
+    @abstractmethod
+    def format(self):
+        ...
+
+
+class MyContent(IContent):
+    def __init__(self, text):
+        super().__init__(text)
+
+    def format(self):
+        return ''.join(['<myML>', self.text, '</myML>'])
+
+
+class IEmail(ABC):
+    @abstractmethod
+    def set_sender(self, sender):
+        pass
+
+    @abstractmethod
+    def set_receiver(self, receiver):
+        pass
+
+    @abstractmethod
+    def set_content(self, content):
+        pass
+
+
+class Email(IEmail):
+    def __init__(self, protocol):
+        self.protocol = protocol
+        self.__sender = None
+        self.__receiver = None
+        self.__content = None
+
+    def set_sender(self, sender):
+        if self.protocol == 'IM':
+            self.__sender = ''.join(["I'm ", sender])
+        else:
+            self.__sender = sender
+
+    def set_receiver(self, receiver):
+        if self.protocol == 'IM':
+            self.__receiver = ''.join(["I'm ", receiver])
+        else:
+            self.__receiver = receiver
+
+    def set_content(self, content):
+        self.__content = content.format()
+
+    def __repr__(self):
+        template = "Sender: {sender}\nReceiver: {receiver}\nContent:\n{content}"
+
+        return template.format(sender=self.__sender, receiver=self.__receiver, content=self.__content)
+
+
+email = Email('IM')
+email.set_sender('qmal')
+email.set_receiver('james')
+content = MyContent('Hello, there!')
+email.set_content(content)
+print(email)
