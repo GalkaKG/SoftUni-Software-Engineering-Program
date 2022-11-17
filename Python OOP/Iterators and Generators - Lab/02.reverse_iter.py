b824a2aba2e7a04bfd8b5009b51b72c3aa6fca29
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2022-11-17 19:08:35 +0200

    Update 02.reverse_iter.py

--- Python OOP/Iterators and Generators - Lab/02.reverse_iter.py
+++ Python OOP/Iterators and Generators - Lab/02.reverse_iter.py
@@ -1,3 +1,23 @@
+# 1-st decision
+
+class reverse_iter:
+    def __init__(self, iterable):
+        self.iterable = iterable
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if not self.iterable:
+            raise StopIteration
+
+        return self.iterable.pop()
+
+    
+
+
+# 2-nd decision:
+
 class reverse_iter:
     def __init__(self, obj):
         self.obj = obj
@@ -15,6 +35,7 @@ class reverse_iter:
             raise StopIteration()
 
 
+# Test code
 reversed_list = reverse_iter([1, 2, 3, 4])
 for item in reversed_list:
     print(item)
