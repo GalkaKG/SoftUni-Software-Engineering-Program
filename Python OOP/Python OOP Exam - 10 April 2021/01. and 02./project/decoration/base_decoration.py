e4b0f323361a3337ef0ed37cf01308f8bd4c9be3
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2022-12-08 11:40:33 +0200

    Create base_decoration.py

---
+++ Python OOP/Python OOP Exam - 10 April 2021/01. and 02./project/decoration/base_decoration.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+
+
+class BaseDecoration(ABC):
+    @abstractmethod
+    def __init__(self, comfort: int, price: float):
+        self.comfort = comfort
+        self.price = price
