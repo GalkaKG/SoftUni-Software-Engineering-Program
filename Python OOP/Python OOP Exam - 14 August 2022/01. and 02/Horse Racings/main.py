2137f1128a625e9a8c930ccb5ada81c313a801ba
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2022-12-09 19:52:01 +0200

    Create main.py

---
+++ Python OOP/Python OOP Exam - 14 August 2022/01. and 02/Horse Racings/main.py
@@ -0,0 +1,15 @@
+from project.horse_race_app import HorseRaceApp
+
+horseRaceApp = HorseRaceApp()
+print(horseRaceApp.add_horse("Appaloosa", "Spirit", 80))
+print(horseRaceApp.add_horse("Thoroughbred", "Rocket", 110))
+print(horseRaceApp.add_jockey("Peter", 19))
+print(horseRaceApp.add_jockey("Mariya", 21))
+print(horseRaceApp.create_horse_race("Summer"))
+print(horseRaceApp.add_horse_to_jockey("Peter", "Appaloosa"))
+print(horseRaceApp.add_horse_to_jockey("Peter", "Thoroughbred"))
+print(horseRaceApp.add_horse_to_jockey("Mariya", "Thoroughbred"))
+print(horseRaceApp.add_jockey_to_horse_race("Summer", "Mariya"))
+print(horseRaceApp.add_jockey_to_horse_race("Summer", "Peter"))
+print(horseRaceApp.add_jockey_to_horse_race("Summer", "Mariya"))
+print(horseRaceApp.start_horse_race("Summer"))
