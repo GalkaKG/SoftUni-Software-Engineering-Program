837a5aaa714db2b28adc233f91499cf58be96e0d
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2022-12-09 20:02:06 +0200

    Create main.py

---
+++ Python OOP/Python OOP Retake Exam - 18 April 2022/01. and 02./Movie App/main.py
@@ -0,0 +1,24 @@
+from project.movie_app import MovieApp
+from project.movie_specification.fantasy import Fantasy
+from project.movie_specification.action import Action
+
+movie_app = MovieApp()
+print(movie_app.register_user('Martin', 24))
+user = movie_app.users_collection[0]
+movie = Action('Die Hard', 1988, user, 18)
+print(movie_app.upload_movie('Martin', movie))
+print(movie_app.movies_collection[0].title)
+print(movie_app.register_user('Alexandra', 25))
+user2 = movie_app.users_collection[1]
+movie2 = Action('Free Guy', 2021, user2, 16)
+print(movie_app.upload_movie('Alexandra', movie2))
+print(movie_app.edit_movie('Alexandra', movie2, title="Free Guy 2"))
+print(movie_app.like_movie('Martin', movie2))
+print(movie_app.like_movie('Alexandra', movie))
+print(movie_app.dislike_movie('Martin', movie2))
+print(movie_app.like_movie('Martin', movie2))
+print(movie_app.delete_movie('Alexandra', movie2))
+movie2 = Fantasy('The Lord of the Rings', 2003, user2, 14)
+print(movie_app.upload_movie('Alexandra', movie2))
+print(movie_app.display_movies())
+print(movie_app)
