3bd9b376245ef60a7009fb95eab7464c9df2ebd3
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2022-12-09 21:45:56 +0200

    Create driver.py

---
+++ Python OOP/Python OOP Exam - 11 December 2021/01. and 02./Christmas Races/driver.py
@@ -0,0 +1,15 @@
+class Driver:
+    def __init__(self, name: str):
+        self.name = name
+        self.car = None
+        self.number_of_wins = 0
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if not value or value.isspace():
+            raise ValueError("Name should contain at least one character!")
+        self.__name = value
