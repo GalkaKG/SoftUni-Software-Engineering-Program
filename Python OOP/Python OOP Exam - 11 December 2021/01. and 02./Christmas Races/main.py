c3f170b85f56623a148b75d1f8e9ab35323b5889
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2022-12-09 21:47:14 +0200

    Create main.py

---
+++ Python OOP/Python OOP Exam - 11 December 2021/01. and 02./Christmas Races/main.py
@@ -0,0 +1,23 @@
+from project.controller import Controller
+
+controller = Controller()
+print(controller.create_driver("Peter"))
+print(controller.create_car("SportsCar", "Porsche 718 Boxster", 470))
+print(controller.add_car_to_driver("Peter", "SportsCar"))
+print(controller.create_car("SportsCar", "Porsche 911", 580))
+print(controller.add_car_to_driver("Peter", "SportsCar"))
+print(controller.create_car("MuscleCar", "BMW ALPINA B7", 290))
+print(controller.create_car("MuscleCar", "Mercedes-Benz AMG GLA 45", 420))
+print(controller.create_driver("John"))
+print(controller.create_driver("Jack"))
+print(controller.create_driver("Kelly"))
+print(controller.add_car_to_driver("Kelly", "MuscleCar"))
+print(controller.add_car_to_driver("Jack", "MuscleCar"))
+print(controller.add_car_to_driver("John", "SportsCar"))
+print(controller.create_race("Christmas Top Racers"))
+print(controller.add_driver_to_race("Christmas Top Racers", "John"))
+print(controller.add_driver_to_race("Christmas Top Racers", "Jack"))
+print(controller.add_driver_to_race("Christmas Top Racers", "Kelly"))
+print(controller.add_driver_to_race("Christmas Top Racers", "Peter"))
+print(controller.start_race("Christmas Top Racers"))
+[print(d.name, d.number_of_wins) for d in controller.drivers]
