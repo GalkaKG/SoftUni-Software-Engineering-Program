8dc8f3a1eda6e76253c2b1e5a5feb3ecb707a5a1
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2022-12-10 21:51:06 +0200

    Create main.py

---
+++ Python OOP/REGULAR EXAM 10 December 2022/1. and 2./Christmas Pastry Shop/main.py
@@ -0,0 +1,16 @@
+from project.christmas_pastry_shop_app import ChristmasPastryShopApp
+
+shop = ChristmasPastryShopApp()
+print(shop.add_delicacy("Gingerbread", "Gingy", 5.20))
+print(shop.delicacies[0].details())
+print(shop.add_booth("Open Booth", 1, 30))
+print(shop.add_booth("Private Booth", 10, 5))
+print(shop.reserve_booth(30))
+print(shop.order_delicacy(1, "Gingy"))
+print(shop.leave_booth(1))
+print(shop.reserve_booth(5))
+print(shop.order_delicacy(1, "Gingy"))
+print(shop.order_delicacy(1, "Gingy"))
+print(shop.order_delicacy(1, "Gingy"))
+print(shop.leave_booth(1))
+print(shop.get_income())
