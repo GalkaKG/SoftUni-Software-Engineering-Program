c79a526666e3d9a6f4c3f9f5ef331c06aa8a65b6
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-02-23 09:52:39 +0200

    Create 05.mathOperations.js

---
+++ JS Front-End/JS Syntax Fundamentals - Lab/05.mathOperations.js
@@ -0,0 +1,26 @@
+function solve(num1, num2, operator) {
+    let result = 0
+
+    switch(operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        case '%':
+            result = num1 % num2;
+            break;
+        case '**':
+            result = num1 ** num2;
+            break;
+    }
+
+    console.log(result)
+}
