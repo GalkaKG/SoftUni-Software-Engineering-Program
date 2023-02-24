453557cc47519a5c1030e4f164d220a67d3dee38
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-02-24 13:16:47 +0200

    Create 05.simpleCalculator.js

---
+++ JS Front-End/Functions and Statements - Lab/05.simpleCalculator.js
@@ -0,0 +1,11 @@
+function order(a,b,input) {
+    if (input === "multiply") {
+        console.log(a * b);
+    } else if (input === "divide") {
+        console.log(a / b);
+    } else if (input === "add") {
+        console.log(a + b);
+    } else {
+        console.log(a - b);
+    }
+}
