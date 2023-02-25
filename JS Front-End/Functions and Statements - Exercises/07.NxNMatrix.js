b66fc417b2d7e0b3a055dedd15004b720f5fed72
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-02-25 11:10:15 +0200

    Create 07.NxNMatrix.js

---
+++ JS Front-End/Functions and Statements - Exercises/07.NxNMatrix.js
@@ -0,0 +1,9 @@
+function matrix(n) {
+    for(let i = 0; i < n; i++) {
+        let output = '';
+        for (let j = 0; j < n; j++) {
+            output += String(n) + ' ';
+        }
+        console.log(output)
+    }
+}
