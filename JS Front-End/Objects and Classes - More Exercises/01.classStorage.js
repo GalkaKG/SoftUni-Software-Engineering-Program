7602479b3143b531d61f8a346cdb3c6c21fb4318
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-03-03 23:30:05 +0200

    Create 01.classStorage.js

---
+++ JS Front-End/Objects and Classes - More Exercises/01.classStorage.js
@@ -0,0 +1,49 @@
+class Storage {
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.storage = [];
+    }
+
+
+    get totalCost () {
+        return this.storage.reduce((previous, el) => {
+            return previous + el.price * el.quantity
+        }, 0);
+    }
+
+    addProduct(product) {
+        this.storage.push(product);
+        this.capacity -= product.quantity;
+        return
+    }
+
+    getProducts() {
+        let output = [];
+        this.storage.forEach(product => output.push(JSON.stringify(product)))
+        return output.join('\n');
+    }
+}
+
+
+// Test 1
+
+let productOne = {name: 'Cucamber', price: 1.50, quantity: 15};
+let productTwo = {name: 'Tomato', price: 0.90, quantity: 25};
+let productThree = {name: 'Bread', price: 1.10, quantity: 8};
+let storage = new Storage(50);
+storage.addProduct(productOne);
+storage.addProduct(productTwo);
+storage.addProduct(productThree);
+console.log(storage.getProducts());
+console.log(storage.capacity);
+console.log(storage.totalCost);
+
+
+// Test 2
+
+let productOne = {name: 'Tomato', price: 0.90, quantity: 19};
+let productTwo = {name: 'Potato', price: 1.10, quantity: 10};
+let storage = new Storage(30);
+storage.addProduct(productOne);
+storage.addProduct(productTwo);
+console.log(storage.totalCost);
