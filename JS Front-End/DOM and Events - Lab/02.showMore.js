bb26ae1935d40f9ea311e16414213a4395b63b1b
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-03-04 11:42:19 +0200

    Create 02.showMore.js

---
+++ JS Front-End/DOM and Events - Lab/02.showMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    let textElement = document.getElementById('text');
+    textElement.style.display = 'inline';
+
+    let showMoreElement = document.getElementById('more');
+    showMoreElement.style.display = 'none';
+}
