339fc93b4d1f7a77e8c337e8284c1a6691b5bdab
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-03-10 21:58:57 +0200

    Create 03.classLaptop.js

---
+++ JS Front-End/Objects and Classes - More Exercises/03.classLaptop.js
@@ -0,0 +1,53 @@
+class Laptop {
+    constructor(info, quality) {
+        this.info = {
+            producer: info.producer,
+            age: info.age,
+            brand: info.brand,
+        };
+        this.quality = quality;
+        this.isOn = false;
+    }
+
+    turnOn() {
+        this.isOn = true;
+        this.quality -= 1;
+    }
+
+    turnOff() {
+        this.isOn = false;
+        this.quality -= 1;
+    }
+
+    showInfo() {
+        return JSON.stringify(this.info);
+    }
+
+    get price() {
+        return (800 - (this.info.age * 2) + (this.quality * 0.5)) 
+    }
+}
+
+
+// Tests:
+
+let info = {producer: "Dell", age: 2, brand: "XPS"}
+let laptop = new Laptop(info, 10)
+laptop.turnOn()
+console.log(laptop.showInfo())
+laptop.turnOff()
+console.log(laptop.quality)
+laptop.turnOn()
+console.log(laptop.isOn)
+console.log(laptop.price)
+
+// let info = {producer: "Lenovo", age: 1, brand: "Legion"}
+// let laptop = new Laptop(info, 10)
+// laptop.turnOn()
+// console.log(laptop.showInfo())
+// laptop.turnOff()
+// laptop.turnOn()
+// laptop.turnOff()
+// console.log(laptop.isOn)
+
+
