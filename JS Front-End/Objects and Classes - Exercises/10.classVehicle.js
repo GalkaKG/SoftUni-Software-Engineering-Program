81bec7cf59b0640aad3c2e94fd6f1da2e3e0894e
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-03-10 10:20:13 +0200

    Update 10.classVehicle.js

--- JS Front-End/Objects and Classes - Exercises/10.classVehicle.js
+++ JS Front-End/Objects and Classes - Exercises/10.classVehicle.js
@@ -14,12 +14,11 @@ class Vehicle {
 }
 
 
-//Tests
+// Tests
 let parts = { engine: 6, power: 100 };
     let vehicle = new Vehicle('a', 'b', parts, 200);
     vehicle.drive(100);
     console.log(vehicle.fuel);
     console.log(vehicle.parts.quality);
     
-// console.log(Vehicle(parts))
 
