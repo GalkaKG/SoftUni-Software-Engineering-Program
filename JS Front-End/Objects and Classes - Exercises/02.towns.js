229c28d6b20175552f0db60475d3d29de3581de0
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-03-10 11:26:43 +0200

    Update 02.towns.js

--- JS Front-End/Objects and Classes - Exercises/02.towns.js
+++ JS Front-End/Objects and Classes - Exercises/02.towns.js
@@ -1,3 +1,4 @@
+// 1-st decision:
 function town(input) {
 
     obj = {};
@@ -11,3 +12,19 @@ function town(input) {
     }
     
 }
+
+
+// 2-nd decision:
+function town(input) {
+    input
+       .map((line) => line.split(' | '))
+       .map(([town, latitude, longitude]) => ({ town, latitude: Number(latitude).toFixed(2), longitude: Number(longitude).toFixed(2) }))
+       .forEach(obj => console.log(obj))
+    
+}
+
+
+// Test
+town (['Sofia | 42.696552 | 23.32601',
+'Beijing | 39.913818 | 116.363625']
+)
