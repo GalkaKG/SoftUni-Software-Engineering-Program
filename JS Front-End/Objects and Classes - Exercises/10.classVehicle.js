def4bfbe840d4d742a1f5e93e15cd62701d450a6
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-03-10 10:19:41 +0200

    Create 10.classVehicle.js

---
+++ JS Front-End/Objects and Classes - Exercises/10.classVehicle.js
@@ -0,0 +1,25 @@
+class Vehicle {
+    constructor(type, model, parts, fuel) {
+        this.type = type;
+        this.model = model;
+        this.parts = parts;
+        this.fuel = fuel;
+        this.quality = this.parts.engine * this.parts.power;
+        this.parts.quality = this.quality
+    }   
+
+    drive(fuelLoss) {
+        this.fuel -= fuelLoss;
+    }
+}
+
+
+//Tests
+let parts = { engine: 6, power: 100 };
+    let vehicle = new Vehicle('a', 'b', parts, 200);
+    vehicle.drive(100);
+    console.log(vehicle.fuel);
+    console.log(vehicle.parts.quality);
+    
+// console.log(Vehicle(parts))
+
