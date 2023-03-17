4f82ef2f918fff0d2c7348cef0c7df495e86bab8
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-03-17 19:54:21 +0200

    Create templates.js

---
+++ JS Front-End/DOM and Events - More Exercises/06. Pascal or Camel Case/templates.js
@@ -0,0 +1,55 @@
+@import url('https://fonts.googleapis.com/css?family=Lato');
+html {
+    font-family: "Lato", sans-serif;
+    font-size: 26px;
+    color: #234465;
+}
+
+form {
+    width: 28%;
+    margin: 5% auto;
+}
+
+form div {
+    margin: 15px 0;
+}
+
+form div label, form div, input {
+    display: block;
+    width: 100%;
+}
+
+label {
+    margin: 8px 0;
+}
+
+input {
+    border: 1px solid #234465;
+    outline: none;
+    border-radius: 6px;
+    padding: 7px;
+}
+
+input[type="button"] {
+    background: #234465;
+    width: 61%;
+    margin: 0 auto;
+    color: white;
+    font-size: 22px;
+    text-transform: uppercase;
+}
+
+input[type="button"]:hover {
+    cursor: pointer;
+    text-decoration: underline;
+}
+
+.result-container {
+    background: #234465;
+    margin: 0 auto;
+    width: 43%;
+    color: white;
+    padding: 25px 0;
+    text-align: center;
+    border-radius: 2px;
+}
