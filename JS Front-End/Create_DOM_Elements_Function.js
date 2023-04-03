de33ad95e3823789af07dfa9c771387927abf9d2
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-04-03 18:11:01 +0300

    Update and rename Create DOM Elements Function to Create_DOM_Elements_Function.js

--- JS Front-End/Create_DOM_Elements_Function.js
+++ JS Front-End/Create_DOM_Elements_Function.js
@@ -35,7 +35,40 @@ function createElement(type, parentNode, content, classes, id, attributes, useIn
     return htmlElement;
   }
   
+
   
+
+// ** SECOND **
   
-  
+//   function createElement(type, content, parentNode, id, classes, attributes){
+//   const htmlElement = document.createElement(type)
+
+//   if(content && type !== 'input'){
+//     htmlElement.textContent = content;
+//   }
+
+//   if(content && type === 'input'){
+//     htmlElement.value = content;
+//   }
+
+//   if(id){
+//     htmlElement.id = id;
+//   }
+
+//   if (classes){
+//     htmlElement.classList.add(...classes);
+//   }
+
+//   if (parentNode){
+//     parentNode.appendChild(htmlElement);
+//     }
+
+//   //{ src: 'link to img', href: 'link to site' }
+//   if (attributes){
+//     for (const key in attributes) {
+//       htmlElement.setAttribute(key, attributes[key]); 
+//     }
+//   } 
+//   return htmlElement
+// }
   
