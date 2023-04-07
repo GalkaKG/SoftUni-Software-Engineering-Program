a6f01c17f52472b27a577867e13f99b1e9da5f9e
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-04-07 13:39:26 +0300

    Create movingTarget.js

---
+++ JS Fundamentals/03. Programming Fundamentals Mid Exam Retake/movingTarget.js
@@ -0,0 +1,64 @@
+function movingTarget(input) {
+    let targets = input.shift().split(' ').map((num) => Number(num));
+
+    let line = input.shift();
+    while (line !== 'End') {
+        let [command, index, other] = line.split(' ');
+        index = Number(index);
+        other = Number(other);
+
+        if (command == 'Shoot') {
+            shoot(index, other);
+        } else if (command == 'Add') {
+            add(index, other);
+        } else if (command == 'Strike') {
+            strike(index, other);
+        }
+
+        line = input.shift();
+    }
+
+    console.log(targets.join('|'));
+
+    function shoot(idx, power) {
+        if (validateIdx(idx)) {
+            targets[idx] -= power;
+        }
+        if (targets[idx] <= 0) {
+            targets.splice(idx, 1);
+        }
+    }
+
+    function add(idx, value) {
+        if (validateIdx(idx)) {
+            return targets.splice(idx, 0, value);
+        } else {
+            console.log('Invalid placement!');
+        }
+    }
+ 
+    function strike(idx, radius) {
+        if (validateIdx(idx) && validateIdx(idx - radius) && validateIdx(idx + radius)) {
+            targets.splice(idx - radius, radius + radius + 1);     
+        } else {
+            console.log('Strike missed!');
+        }
+    }
+
+    function validateIdx(idx) {
+        return idx >= 0 && idx < targets.length;
+    }
+}
+
+movingTarget((["52 74 23 44 96 110",
+"Shoot 5 10",
+"Shoot 1 80",
+"Strike 2 1",
+"Add 22 3",
+"End"])
+);
+
+// movingTarget((["1 2 3 4 5",
+// "Strike 0 1",
+// "End"])
+// )
