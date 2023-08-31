a3f4b255715b09b5abe51a89d1bf40800b33c8bb
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-08-31 14:05:06 +0300

    Update 01.areas_in_matrix.py

--- Algorithms (with Python) Fundamentals - Exercises/Graph Theory, Traversal and Topological Sorting - Exercise/01.areas_in_matrix.py
+++ Algorithms (with Python) Fundamentals - Exercises/Graph Theory, Traversal and Topological Sorting - Exercise/01.areas_in_matrix.py
@@ -1,3 +1,50 @@
+# 1-st solution
+rows = int(input())
+cols = int(input())
+
+matrix = [list(input()) for _ in range(rows)]
+visited = [[False] * cols for _ in range(rows)]
+
+result = {}
+
+
+def dfs(curr_node, row, col, matrix, visited, result):
+    if row < 0 or col < 0 or row >= len(matrix) or col >= len(matrix[0]):
+        return 0
+    if visited[row][col]:
+        return 0
+    if matrix[row][col] != curr_node:
+        return 0
+
+    visited[row][col] = True
+    dfs(curr_node, row - 1, col, matrix, visited, result)
+    dfs(curr_node, row + 1, col, matrix, visited, result)
+    dfs(curr_node, row, col - 1, matrix, visited, result)
+    dfs(curr_node, row, col + 1, matrix, visited, result)
+
+    return 1
+
+
+total_areas = 0
+for row in range(rows):
+    for col in range(cols):
+        curr_node = matrix[row][col]
+        value = dfs(curr_node, row, col, matrix, visited, result)
+        total_areas += value
+
+        if curr_node not in result:
+            result[curr_node] = 0
+        result[curr_node] += value
+
+
+print(f'Areas: {total_areas}')
+[print(f"Letter '{k}' -> {v}") for k, v in sorted(result.items())]
+# for k, v in result.items():
+#     print(f"Letter '{k}' -> {v}")
+
+
+
+# 2-nd solution
 def dfs(parent, row, col, matrix, visited):
     if row < 0 or col < 0 or row >= len(matrix) or col >= len(matrix[0]):
         return
