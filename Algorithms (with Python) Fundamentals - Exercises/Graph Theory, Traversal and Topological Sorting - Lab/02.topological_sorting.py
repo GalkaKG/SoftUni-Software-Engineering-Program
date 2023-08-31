ed392799320c70b2b7533113f4b6354d815f040c
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-08-31 13:20:43 +0300

    Update 02.topological_sorting.py

--- Algorithms (with Python) Fundamentals - Exercises/Graph Theory, Traversal and Topological Sorting - Lab/02.topological_sorting.py
+++ Algorithms (with Python) Fundamentals - Exercises/Graph Theory, Traversal and Topological Sorting - Lab/02.topological_sorting.py
@@ -1,3 +1,47 @@
+# 1-st decision
+def sorted_nodes(graph, predecessors, output):
+    while predecessors:
+        nodes_to_remove = [k for k in predecessors if predecessors[k] == 0]
+        if not nodes_to_remove:
+            return False
+        node_to_remove = nodes_to_remove[0]
+        del predecessors[node_to_remove]
+        output.append(node_to_remove)
+        for child in graph[node_to_remove]:
+            predecessors[child] -= 1
+
+    return True
+
+
+n = int(input())
+
+graph = {}
+for _ in range(n):
+    key, value = input().split(' ->')
+    graph[key] = list(value.strip().split(', ')) if value and value != ' ' else []
+
+predecessors = {}
+
+for node, children in graph.items():
+    if node not in predecessors:
+        predecessors[node] = 0
+    for child in children:
+        if child not in predecessors:
+            predecessors[child] = 1
+        else:
+            predecessors[child] += 1
+
+output = []
+
+is_sortable = sorted_nodes(graph, predecessors, output)
+if is_sortable:
+    print(f'Topological sorting: {", ".join(str(x) for x in output)}')
+else:
+    print('Invalid topological sorting')
+
+
+
+# 2-nd decision
 def find_dependencies(graph):
     result = {}
     for node, children in graph.items():
