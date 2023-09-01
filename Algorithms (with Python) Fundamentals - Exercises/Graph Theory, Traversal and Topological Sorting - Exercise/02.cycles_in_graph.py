0317ce8aacd6f07f965a8cab5ae44a0227c72c06
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-09-01 12:14:55 +0300

    Update 02.cycles_in_graph.py

--- Algorithms (with Python) Fundamentals - Exercises/Graph Theory, Traversal and Topological Sorting - Exercise/02.cycles_in_graph.py
+++ Algorithms (with Python) Fundamentals - Exercises/Graph Theory, Traversal and Topological Sorting - Exercise/02.cycles_in_graph.py
@@ -1,3 +1,44 @@
+# 1-st solution
+def dfs(graph, node, visited, cycles):
+    if node in cycles:
+        raise Exception
+    if node in visited:
+        return
+
+    visited.append(node)
+    cycles.append(node)
+
+    for child in graph[node]:
+        dfs(graph, child, visited, cycles)
+
+    cycles.remove(node)
+
+
+graph = {}
+
+while True:
+    line = input()
+    if line == 'End':
+        break
+
+    key, value = line.split('-')
+    if key not in graph:
+        graph[key] = []
+    if value not in graph:
+        graph[value] = []
+    graph[key].append(value)
+
+try:
+    visited = []
+    for node in graph:
+        dfs(graph, node, visited, [])
+    print('Acyclic: Yes')
+except Exception:
+    print('Acyclic: No')
+
+
+
+# 2-nd solution
 def dfs(node, graph, visited, cycles):
     if node in cycles:
         raise Exception
