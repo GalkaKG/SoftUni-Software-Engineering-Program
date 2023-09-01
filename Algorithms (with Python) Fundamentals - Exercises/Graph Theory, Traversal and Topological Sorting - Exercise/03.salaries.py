60827e9f6edcbc3f2df819843177e1524a5f7e4f
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-09-01 12:56:14 +0300

    Update 03.salaries.py

--- Algorithms (with Python) Fundamentals - Exercises/Graph Theory, Traversal and Topological Sorting - Exercise/03.salaries.py
+++ Algorithms (with Python) Fundamentals - Exercises/Graph Theory, Traversal and Topological Sorting - Exercise/03.salaries.py
@@ -1,3 +1,40 @@
+# 1-st solution
+def dfs(node, graph, visited, node_salary):
+    if visited[node]:
+        return node_salary[node]
+
+    visited[node] = True
+
+    if not graph[node]:
+        node_salary[node] = 1
+        return node_salary[node]
+
+    if node not in node_salary:
+        node_salary[node] = 0
+
+    for child in graph[node]:
+        node_salary[node] += dfs(child, graph, visited, node_salary)
+
+    return node_salary[node]
+
+
+count_nodes = int(input())
+graph = {}
+
+for key in range(count_nodes):
+    graph[key] = [node for node, child in enumerate(list(input())) if child == 'Y']
+
+sum_salaries = 0
+node_salary = {}
+visited = [False] * len(graph)
+
+for node in graph:
+    sum_salaries += dfs(node, graph, visited, node_salary)
+
+print(sum_salaries)
+
+
+# 2-nd solution
 def dfs(node, graph, salaries):
     if salaries[node] is not None:
         return salaries[node]
