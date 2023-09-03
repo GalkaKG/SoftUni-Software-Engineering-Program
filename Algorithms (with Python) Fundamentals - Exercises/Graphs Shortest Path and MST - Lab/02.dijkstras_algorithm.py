ff589b3ecbca0d162fe7f0579ed83b236cbfebfe
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-09-03 15:20:22 +0300

    Update 02.dijkstras_algorithm.py

--- Algorithms (with Python) Fundamentals - Exercises/Graphs Shortest Path and MST - Lab/02.dijkstras_algorithm.py
+++ Algorithms (with Python) Fundamentals - Exercises/Graphs Shortest Path and MST - Lab/02.dijkstras_algorithm.py
@@ -1,3 +1,66 @@
+# 1-st solution
+from collections import deque
+from queue import PriorityQueue
+
+
+class Edge:
+    def __init__(self, start, end, weight):
+        self.start = start
+        self.end = end
+        self.weight = weight
+
+
+count_edges = int(input())
+graph = {}
+
+for _ in range(count_edges):
+    start, end, weight = [int(x) for x in input().split(', ')]
+    if start not in graph:
+        graph[start] = []
+    if end not in graph:
+        graph[end] = []
+    graph[start].append(Edge(start, end, weight))
+
+start_node = int(input())
+end_node = int(input())
+
+max_node = max(graph.keys()) + 1
+parent = [None] * max_node
+distance = [float('inf')] * max_node
+
+distance[start_node] = 0
+pq = PriorityQueue()
+pq.put((0, start_node))
+
+while not pq.empty():
+    weight, node = pq.get()
+    if node == end_node:
+        break
+    for child in graph[node]:
+        new_distance = weight + child.weight
+        if new_distance < distance[child.end]:
+            distance[child.end] = new_distance
+            parent[child.end] = node
+            pq.put((new_distance, child.end))
+
+
+if distance[end_node] == float('inf'):
+    print('There is no such path.')
+else:
+    print(distance[end_node])
+
+    path = deque()
+    end_idx = end_node
+    path.appendleft(end_idx)
+
+    while start_node != end_idx:
+        path.appendleft(parent[end_idx])
+        end_idx = parent[end_idx]
+
+    print(*path)
+
+
+# 2-nd solution
 from queue import PriorityQueue
 from collections import deque
 
