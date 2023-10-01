83c67b7f0d923eefc3e7bfc286245dc90b34e0b3
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-10-01 21:32:35 +0300

    Create 02.nums_1_to_10.cpp

---
+++ C++ Basics/First Steps In Coding - Lab/02.nums_1_to_10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << 1 << endl;
+    cout << 2 << endl;
+    cout << 3 << endl;
+    cout << 4 << endl;
+    cout << 5 << endl;
+    cout << 6 << endl;
+    cout << 7 << endl;
+    cout << 8 << endl;
+    cout << 9 << endl;
+    cout << 10 << endl;
+}
