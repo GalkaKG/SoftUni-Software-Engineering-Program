af047f34a418bd8fff7d73f83348e2a18fdad7bb
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-10-08 15:18:46 +0300

    Add files via upload

---
+++ Containers and Cloud/Exercise: Infrastructure as Code/1.Azure Resource Group/main.tf
@@ -0,0 +1,20 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "galkaorg"
+  location = "West Europe"
+}
