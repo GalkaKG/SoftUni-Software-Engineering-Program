62f87ba82796105ba413f4e282a4597b1bd10c2d
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-10-31 21:48:33 +0200

    Create 02.reverse_an_array_of_strings.php

---
+++ PHP Web Development Basics/Advanced Syntax and Operations - Exercise/02.reverse_an_array_of_strings.php
@@ -0,0 +1,21 @@
+// First solution
+
+<?php
+// Read the array of strings
+echo "Enter a sequence of space-separated strings: ";
+$input = trim(fgets(STDIN));
+
+// Split the input string into an array of strings
+$strings = explode(' ', $input);
+
+// Reverse the array manually
+$length = count($strings);
+for ($i = 0; $i < $length / 2; $i++) {
+    $temp = $strings[$i];
+    $strings[$i] = $strings[$length - $i - 1];
+    $strings[$length - $i - 1] = $temp;
+}
+
+// Print the reversed array
+echo implode(' ', $strings) . PHP_EOL;
+?>
