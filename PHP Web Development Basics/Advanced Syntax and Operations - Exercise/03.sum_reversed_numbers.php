da0fbab68fd8b625342c789d7a8223499c17f0b2
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2023-11-02 20:12:46 +0200

    Create 03.sum_reversed_numbers.php

---
+++ PHP Web Development Basics/Advanced Syntax and Operations - Exercise/03.sum_reversed_numbers.php
@@ -0,0 +1,30 @@
+<?php
+// Read the array of integers
+echo "Enter integers (space-separated): ";
+$input = trim(fgets(STDIN));
+
+// Split the input string into an array of integers
+$numbers = array_map('intval', explode(' ', $input));
+
+// Initialize a variable to store the total sum
+$totalSum = 0;
+
+// Iterate through the array, rotating it after each iteration
+foreach ($numbers as $number) {
+    $totalSum += reverseAndSum($number);
+}
+
+// Function to reverse a number and return the sum
+function reverseAndSum($number) {
+    $reversedNumber = 0;
+    while ($number > 0) {
+        $digit = $number % 10;
+        $reversedNumber = $reversedNumber * 10 + $digit;
+        $number = (int)($number / 10);
+    }
+    return $reversedNumber;
+}
+
+// Print the total sum
+echo $totalSum . PHP_EOL;
+?>
